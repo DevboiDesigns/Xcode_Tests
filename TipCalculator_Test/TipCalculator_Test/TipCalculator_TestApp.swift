f927426cadea9e3c8038e2860523c551d26b772e
Christopher Hicks <visrule@protonmail.com>
2022-04-21 20:24:51 -0500

    base project

---
+++ TipCalculator_Test/TipCalculator_Test/TipCalculator_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculator_TestApp.swift
+//  TipCalculator_Test
+//
+//  Created by Christopher Hicks on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculator_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
